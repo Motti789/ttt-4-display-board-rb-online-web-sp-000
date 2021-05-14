aa918e5084a619cb04e30a2882eea9a8051688fa
Motti Wilschanski <mchillywilly@gmail.com>
2021-05-14 20:21:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,3 +6,26 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+=======
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = [" "," "," "," ","X"," "," "," "," "]
+display_board(board)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+board = ["O"," "," "," "," "," "," "," "," "]
+board[0] = "O"
+display_board(board)
+>>>>>>> 96bc66137bbc7014b07acd58b59c912a3b2699ae
